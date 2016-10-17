169a3fef22dda01582a2db349e27bf2b4a37057c
Archy <444163419@qq.com>
2016-10-17 19:50:20 +0800

    添加容错处理

--- SinaWeibo_swift/SinaWeibo_swift/Classess/Main/MainViewController.swift
+++ SinaWeibo_swift/SinaWeibo_swift/Classess/Main/MainViewController.swift
@@ -24,18 +24,41 @@ extension MainViewController {
 
   func  addChildviewControllers()
     {
-    addChildViewController(HomeViewController(), title: "首页", imageName: "tabbar_home")
-        addChildViewController(MessageViewController(), title: "消息", imageName: "tabbar_message_center")
-        addChildViewController(DisCoverViewController(), title: "发现", imageName: "tabbar_discover")
-        addChildViewController(ProfileViewController(), title: "我", imageName: "tabbar_profile")
+    addChildViewController("HomeViewController", title: "首页", imageName: "tabbar_home")
+        addChildViewController("MessageViewController", title: "消息", imageName: "tabbar_message_center")
+        addChildViewController("DisCoverViewController", title: "发现", imageName: "tabbar_discover")
+        addChildViewController("ProfileViewController", title: "我", imageName: "tabbar_profile")
     }
 }
 
 extension MainViewController {
 
-    private func addChildViewController(childVC: UIViewController , title :String , imageName : String) {
+    private func addChildViewController(childVCname: String , title :String , imageName : String) {
+        //0.获取命名空间
+        guard let nameSpace = NSBundle.mainBundle().infoDictionary!["CFBundleExecutable"] as? String
+        else {
+        print("没有获取到命名空间")
+            return
+        }
         
-        //设置自控制器的属性
+        //1.根据字符串获取对应的Class
+        guard let ChildVcClass = NSClassFromString(nameSpace + "." + childVCname)
+        else
+        {
+            print("没有获取到字符串对应的class")
+            return
+        }
+        
+        //2.将对应的AnyObject转成控制器的类型
+        
+        guard let childVcType = ChildVcClass as? UIViewController.Type else {
+         print("没有获取对应控制器类型")
+            return
+        }
+        //3.创建对应控制器对象
+        let childVC = childVcType.init()
+        
+        //4.设置自控制器的属性
         childVC.title = title;
         childVC.tabBarItem.image = UIImage(named: imageName)
         childVC.tabBarItem.selectedImage = UIImage(named: imageName + "_highlighted")
