f5c91971c8d48ce22d4ce57897874f4acfda0a8c
archy <444163419@qq.com>
2016-10-21 16:19:43 +0800

    bug修复

--- SinaWeibo_swift/SinaWeibo_swift/Classess/Home/Popover/PopoverViewController.swift
+++ SinaWeibo_swift/SinaWeibo_swift/Classess/Home/Popover/PopoverViewController.swift
@@ -2,7 +2,7 @@
 //  PopoverViewController.swift
 //  SinaWeibo_swift
 //
-//  Created by archy on 16/10/20.
+//  Created by archy on 16/10/21.
 //  Copyright © 2016年 archy. All rights reserved.
 //
 
@@ -10,9 +10,7 @@ import UIKit
 
 class PopoverViewController: UIViewController {
 
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = UIColor.orangeColor()
     }
 }
