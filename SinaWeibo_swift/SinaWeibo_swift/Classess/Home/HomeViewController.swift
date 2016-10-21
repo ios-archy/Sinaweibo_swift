e0e599603342599506294ed07aaf08ef6deb4e40
archy <444163419@qq.com>
2016-10-21 18:22:02 +0800

    自定义转场动画抽取

--- SinaWeibo_swift/SinaWeibo_swift/Classess/Home/HomeViewController.swift
+++ SinaWeibo_swift/SinaWeibo_swift/Classess/Home/HomeViewController.swift
@@ -14,7 +14,7 @@ class HomeViewController: BaseViewController {
     var isPresented : Bool = false
      //MARK: --懒加载属性
     private lazy var titleBtn : TitleButton = TitleButton()
-    
+    private lazy var popoviewAnimator : PopoverAnimator = PopoverAnimator()
      //MARK: --系统回调函数
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -73,107 +73,13 @@ extension HomeViewController {
         popoverVc.modalPresentationStyle = .Custom
         
         //设置转场代理
-        popoverVc.transitioningDelegate = self
+        popoverVc.transitioningDelegate = popoviewAnimator
         //4.弹出控制器
         presentViewController(popoverVc, animated: true, completion: nil)
     }
 }
 
- //MARK: --自定义转场代理的方法
-extension HomeViewController :UIViewControllerTransitioningDelegate {
-
-    //目的：改变弹出View的尺寸
-    func presentationControllerForPresentedViewController(presented: UIViewController, presentingViewController presenting: UIViewController, sourceViewController source: UIViewController) -> UIPresentationController? {
-        return ArchyPresentationController(presentedViewController: presented,presentingViewController: presenting)
-    }
-    
-    //目的：自定义弹出的动画
-    func animationControllerForPresentedController(presented: UIViewController, presentingController presenting: UIViewController, sourceController source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
-        isPresented = true
-        return self
-    }
-    
-    //目的:自定义消失的动画
-    func animationControllerForDismissedController(dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
-        isPresented = false
-        return self
-    }
-    
-}
-
-
-
- //MARK: --弹出和消失动画代理的方法
-extension HomeViewController :   UIViewControllerAnimatedTransitioning {
-
-    
-    /**
-    *  动画执行的时间
-    */
-    func transitionDuration(transitionContext: UIViewControllerContextTransitioning?) -> NSTimeInterval {
-        return 0.5
-    }
-    
-    
-    //获取转场上下文：可以通过转场上下文获取弹出的view和消失的view
-    // UITransitionContextFromViewKey : 获取消失的View
-    // UITransitionContextToViewKey : 获取弹出的View
-    func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
-        
-        isPresented ? animationForPresentedView( transitionContext ) : animationForDismissedView( transitionContext )
-    }
-    
-    
-    
-    /**
-    *  自定义弹出动画
-    */
-    private func animationForPresentedView(transitionContext :UIViewControllerContextTransitioning)
-    {
-        
-        //1.获取弹出的view
-        let presentedView = transitionContext.viewForKey(UITransitionContextToViewKey)!
-        
-        //2.将弹出的View添加到ContainerView中
-        transitionContext.containerView()?.addSubview(presentedView)
-        
-        //3.执行动画
-        presentedView.transform = CGAffineTransformMakeScale(1.0, 0.0)
-        presentedView.layer.anchorPoint = CGPointMake(0.5, 0)
-        
-        UIView.animateWithDuration(transitionDuration(transitionContext), animations: { () -> Void in
-            presentedView.transform = CGAffineTransformIdentity
-            }) { (_) -> Void in
-                //必须告诉转场上下文你已经完成动画
-                transitionContext.completeTransition(true)
-        }
-        
-        
-    }
-    
-    /**
-    *  自定义消失动画
-    */
-    private func animationForDismissedView(transitionContext : UIViewControllerContextTransitioning) {
-    
-        //1.获取消失的View
-        let dismissView = transitionContext.viewForKey(UITransitionContextFromViewKey)
-        
-        //2.执行动画
-        UIView.animateWithDuration(transitionDuration(transitionContext), animations: { () -> Void in
-            dismissView?.transform = CGAffineTransformMakeScale(1.0, 0.00001)
-            }) { (_) -> Void in
-                dismissView?.removeFromSuperview()
-                //必须告诉转场上下文你已经完成了动画
-                transitionContext.completeTransition(true)
-        }
-    }
-    
-    
-}
-
-
-
+ 
 
 
 
---
+++ SinaWeibo_swift/SinaWeibo_swift/Classess/Home/Popover/PopoverAnimator.swift
@@ -0,0 +1,111 @@
+//
+//  PopoverAnimator.swift
+//  SinaWeibo_swift
+//
+//  Created by archy on 16/10/21.
+//  Copyright © 2016年 archy. All rights reserved.
+//
+
+import UIKit
+
+class PopoverAnimator: NSObject {
+
+    var isPresent : Bool = false
+    
+}
+
+
+ //MARK: --自定义转场代理的方法
+extension PopoverAnimator : UIViewControllerTransitioningDelegate {
+
+    //目的：改变弹出的view的尺寸
+    
+    func presentationControllerForPresentedViewController(presented: UIViewController, presentingViewController presenting: UIViewController, sourceViewController source: UIViewController) -> UIPresentationController? {
+        return ArchyPresentationController(presentedViewController : presented ,presentingViewController: presenting )
+    }
+    
+    // //MARK: --目的：自定义弹出的动画
+    func animationControllerForPresentedController(presented: UIViewController, presentingController presenting: UIViewController, sourceController source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        
+        isPresent = true
+         return self
+    }
+    
+    
+     //MARK: --自定义消失的动画
+    func animationControllerForDismissedController(dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        isPresent = false
+        return self
+    }
+}
+
+ //MARK: --弹出和消失动画代理的方法
+extension PopoverAnimator : UIViewControllerAnimatedTransitioning {
+
+    /**
+    *  动画执行的时间
+    */
+    
+    func transitionDuration(transitionContext: UIViewControllerContextTransitioning?) -> NSTimeInterval {
+        return 0.5
+    }
+    
+    
+    //
+    func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
+        
+        isPresent ? animationForPresentedView(transitionContext) : animationForDismissedView(transitionContext)
+    }
+    
+    
+    //自定义弹出动画
+    private func animationForPresentedView(transitonContext : UIViewControllerContextTransitioning) {
+    //1.获取弹出的view
+        let presentedView = transitonContext.viewForKey(UITransitionContextToViewKey)!
+        
+        //2.将弹出的view添加到contaninerView中
+        transitonContext.containerView()?.addSubview(presentedView)
+        
+        
+        //3.执行动画
+        presentedView.transform = CGAffineTransformMakeScale(1.0, 0.0)
+        presentedView.layer.anchorPoint = CGPointMake(0.5, 0)
+        UIView.animateWithDuration(transitionDuration(transitonContext), animations: { () -> Void in
+            presentedView.transform = CGAffineTransformIdentity
+            }) { (_) -> Void in
+                //必须告诉转场上下文你已经完成动画
+                transitonContext.completeTransition(true)
+        }
+        
+    
+    }
+    
+    /**
+    *  自定义消失动画
+    */
+    private func animationForDismissedView(transitionContext : UIViewControllerContextTransitioning) {
+    //1.获取消失view
+        let dismissView  = transitionContext.viewForKey(UITransitionContextFromViewKey)
+        
+        //2.执行动画
+        UIView.animateWithDuration(transitionDuration(transitionContext), animations: { () -> Void in
+            dismissView?.transform = CGAffineTransformMakeScale(1.0, 0.00001)
+            }) { (_) -> Void in
+                dismissView?.removeFromSuperview()
+                //必须告诉转场上下文你已经完成动画
+                transitionContext.completeTransition(true)
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
