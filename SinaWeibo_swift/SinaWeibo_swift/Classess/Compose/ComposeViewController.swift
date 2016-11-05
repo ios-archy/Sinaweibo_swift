d04dd96114caaf3371ddeaf04711c546b8137a9f
Archy <444163419@qq.com>
2016-11-05 23:55:39 +0800

    添加发布控制器

---
+++ SinaWeibo_swift/SinaWeibo_swift/Classess/Compose/ComposeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ComposeViewController.swift
+//  SinaWeibo_swift
+//
+//  Created by yuqi on 16/11/5.
+//  Copyright © 2016年 archy. All rights reserved.
+//
+
+import UIKit
+
+class ComposeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
